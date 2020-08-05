2bbb4a37598cd8a0d1489a2d0b7eba0b0b93f691
HingTang <hing.tang.1115@gmail.com>
2020-08-05 14:58:25 +0700

    Display Dialog to get the lifecycle log

--- app/lifecycle/src/main/java/com/hing/lifecycle/FirstActivity.kt
+++ app/lifecycle/src/main/java/com/hing/lifecycle/FirstActivity.kt
@@ -1,9 +1,15 @@
 package com.hing.lifecycle
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.hing.lifecycle.utils.Utils.debugLog
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
 
@@ -11,6 +17,46 @@ class FirstActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
         debugLog("${javaClass.simpleName} - onCreate + $savedInstanceState")
+
+        btnGetPermissionDialog.setOnClickListener {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                EXTERNAL_PERMISSION_CODE
+            )
+        }
+
+        btnAlertDialog.setOnClickListener {
+            val dialog = AlertDialog.Builder(this).apply {
+                title = ""
+                setMessage("This is an Alert Dialog")
+                create()
+            }
+
+            dialog.show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        debugLog("${javaClass.simpleName} - onRequestPermissionsResult requestCode: $requestCode")
+        when (requestCode) {
+            EXTERNAL_PERMISSION_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 
     override fun onRestart() {
@@ -64,5 +110,9 @@ class FirstActivity : AppCompatActivity() {
     ) {
         super.onRestoreInstanceState(savedInstanceState, persistentState)
         debugLog("${javaClass.simpleName} - onRestoreInstanceState2 $savedInstanceState - $persistentState")
+    }
+
+    private companion object {
+        const val EXTERNAL_PERMISSION_CODE = 100
     }
 }
